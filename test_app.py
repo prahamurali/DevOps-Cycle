e2f7941bd8a2c4bbdca95660896a80f22698911a
Prahalathan <praha24@gmail.com>
2020-07-20 11:41:22 +0530

    Modified test file

--- test_app.py
+++ test_app.py
@@ -7,6 +7,7 @@ class TestdbConn(unittest.TestCase):
     def test_dbconn(self):
         res = dbconn()
         self.assertEqual(res, True)
+        return "Test Passed"
 
 
 if __name__ == '__main__':
